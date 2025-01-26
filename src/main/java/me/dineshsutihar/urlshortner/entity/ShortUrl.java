f7d8bd2aab98413a6b4bf7fb2d1b85e123ba3261
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2025-01-26 13:47:33 +0530

    add: both user and shorturl entity added

--- src/main/java/me/dineshsutihar/urlshortner/entity/ShortUrl.java
+++ src/main/java/me/dineshsutihar/urlshortner/entity/ShortUrl.java
@@ -1,4 +1,33 @@
 package me.dineshsutihar.urlshortner.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "short_urls")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ShortUrl {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String originalUrl;;
+
+    @Column(nullable = false, unique = true)
+    private String shortUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    private User user;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+
 }
--- src/main/java/me/dineshsutihar/urlshortner/entity/User.java
+++ src/main/java/me/dineshsutihar/urlshortner/entity/User.java
@@ -1,4 +1,31 @@
 package me.dineshsutihar.urlshortner.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
+
+@Entity
+@Table(name="users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique=true, nullable=false)
+    private String email;
+
+    @Column(nullable=false)
+    private String password;
+
+    private String name;
+    private String profileUrl;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ShortUrl> shortUrls;
 }
