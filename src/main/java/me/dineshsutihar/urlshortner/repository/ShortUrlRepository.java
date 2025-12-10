da4b5a3ea576b9ccb6f5f2fd0a5f1a9c860aee8c
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2025-12-10 09:45:00 +0530

    feat: add findByUser, countByUser, sumClickCount queries to ShortUrlRepository

--- src/main/java/me/dineshsutihar/urlshortner/repository/ShortUrlRepository.java
+++ src/main/java/me/dineshsutihar/urlshortner/repository/ShortUrlRepository.java
@@ -2,10 +2,17 @@ package me.dineshsutihar.urlshortner.repository;
 
 import java.util.List;
 import me.dineshsutihar.urlshortner.entity.ShortUrl;
+import me.dineshsutihar.urlshortner.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface ShortUrlRepository extends JpaRepository<ShortUrl, Long> {
     Optional<ShortUrl> findByShortCode(String shortCode);
+    List<ShortUrl> findByUserOrderByCreatedAtDesc(User user);
+    long countByUser(User user);
+    boolean existsByShortCode(String shortCode);
 
+    @Query("SELECT SUM(s.clickCount) FROM ShortUrl s WHERE s.user = :user")
+    Long sumClickCountByUser(User user);
 }
